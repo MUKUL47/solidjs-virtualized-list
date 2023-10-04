ca97e8fb12e6f95acac5951731f18de2c57c1a65
Mukul Dutt <mukuldutt1234@gmail.com>
2023-10-05 01:23:37 +0200

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,44 @@
+/* @refresh reload */
+import { createSignal } from "solid-js";
+import { render } from "solid-js/web";
+import { VirtualizedList } from "./virtualized-list";
+
+const root = document.getElementById("root");
+let data = Array.from({ length: 1000000 }).map((v, i) => i + 1);
+let scrollTo;
+const [scrollToKey, setScrollToKey] = createSignal(0);
+render(
+  () => (
+    <div>
+      <VirtualizedList
+        options={{
+          containerSize: 500,
+          dataLength: data.length,
+          cellHeightWidth: 20,
+          overscan: 10,
+          direction: "y",
+          scrollTo: (cb) => (scrollTo = cb),
+          onScroll: (e) => {
+            console.log(e);
+          },
+        }}
+        parentContainerStyle={{
+          width: "250px",
+        }}
+      >
+        {({ index }) => {
+          return data[index] ? (
+            <p style={{ height: "100%" }}>!{data[index]}</p>
+          ) : null;
+        }}
+      </VirtualizedList>
+      <input
+        type="number"
+        placeholder="Index"
+        onChange={(e) => setScrollToKey(() => e.target.value)}
+      />
+      <button onClick={() => scrollTo(+scrollToKey())}>Scroll</button>
+    </div>
+  ),
+  root
+);
---
+++ src/virtualized-list.jsx
@@ -0,0 +1,161 @@
+import { createSignal, onMount } from "solid-js";
+/**
+ * VirtualizedList component renders a virtualized list of items for optimized performance.
+ * @param {{
+ *  children: ({ index: number }) => JSX.Element;
+ *  options: {
+ *    containerSize: number;
+ *    overscan?: number;
+ *    dataLength: number;
+ *    cellHeightWidth: number;
+ *    direction?: "x" | "y";
+ *    scrollTo?: (index: number) => void;
+ *    debounce?: number;
+ *    onScroll?: (event: Event) => void;
+ *  };
+ *  parentContainerStyle?: Record<string, string>;
+ *  scrollContainerStyle?: Record<string, string>;
+ * }} props
+ * @returns {JSX.Element}
+ */
+export function VirtualizedList({
+  children,
+  options,
+  parentContainerStyle = {},
+  scrollContainerStyle = {},
+} = {}) {
+  const [list, setList] = createSignal();
+  let debouncer = -1;
+  let parentRef, scrollerRef;
+  const {
+    containerSize,
+    overscan,
+    dataLength,
+    cellHeightWidth,
+    direction = "y",
+    scrollTo,
+    debounce,
+    onScroll,
+  } = options || {};
+  const untilIndex = Math.round(containerSize / cellHeightWidth);
+  const heightWidth = direction === "y" ? "height" : "width";
+  const styleCoordiateProperty = direction === "y" ? "top" : "left";
+  onMount(() => {
+    if (
+      !options ||
+      !options.containerSize ||
+      !options.dataLength ||
+      !options.cellHeightWidth
+    ) {
+      throw new Error("VirtualizedList: Required options are missing.");
+    }
+
+    initialize();
+    update(true);
+    scrollTo?.((index) => scrollToScroller(index));
+  });
+
+  function scrollToScroller(index) {
+    index = +index;
+    if (index < 0 || index > dataLength || isNaN(index)) return;
+    parentRef.scrollTo({
+      [styleCoordiateProperty]: index * cellHeightWidth,
+    });
+    update(false, index);
+  }
+
+  function getDataFromChild(index) {
+    try {
+      return children?.({
+        index,
+      });
+    } catch (e) {
+      return null;
+    }
+  }
+
+  function update(isInit, scrollTo) {
+    const lists = [];
+    const [from, until] = getIterable(!!isInit, scrollTo);
+    for (let i = from; i < until; i++) {
+      const node = getDataFromChild(i);
+      if (!node) continue;
+      lists.push(
+        getNode({
+          height: px(cellHeightWidth),
+          styleCoordiate: px(cellHeightWidth * i),
+          node,
+        })
+      );
+    }
+    setList(lists);
+  }
+
+  function getNode({ height, styleCoordiate, node }) {
+    const dirCoords = { [styleCoordiateProperty]: styleCoordiate };
+    return (
+      <div
+        style={{
+          height,
+          ...dirCoords,
+          position: "absolute",
+        }}
+      >
+        {node}
+      </div>
+    );
+  }
+
+  function getIterable(isInit, scrollTo) {
+    if (isInit) return [0, untilIndex + (overscan || 0)];
+    const startIndex =
+      scrollTo === undefined
+        ? Math.round(
+            Math.abs(
+              scrollerRef.getBoundingClientRect()[styleCoordiateProperty]
+            ) / cellHeightWidth
+          )
+        : scrollTo;
+    return [
+      startIndex - (overscan || 0),
+      startIndex + untilIndex + (overscan || 0),
+    ];
+  }
+
+  function onScrollRef(e) {
+    if (debounce) {
+      clearTimeout(debouncer);
+      debouncer = setTimeout(() => update(), debounce);
+      onScroll?.(e);
+      return;
+    }
+    onScroll?.(e);
+    update();
+  }
+
+  function initialize() {
+    parentRef.style[heightWidth] = px(containerSize);
+    scrollerRef.style[heightWidth] = px(cellHeightWidth * dataLength);
+  }
+
+  return (
+    <div
+      ref={parentRef}
+      onScroll={onScrollRef}
+      style={{
+        ...parentContainerStyle,
+        overflow: "auto",
+        willChange: "transform",
+        position: "relative",
+        boxSizing: "border-box",
+      }}
+    >
+      <div ref={scrollerRef} style={scrollContainerStyle}>
+        {list()}
+      </div>
+    </div>
+  );
+}
+function px(v) {
+  return `${v}px`;
+}
